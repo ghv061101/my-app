906e058e9dbe8f98f74a357686034c49207dd641
Harsha <ghv061101@gmail.com>
2024-02-03 16:41:05 +0530

    ok

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+
+
+function App() {
+  return (
+    <div style={{display:"flex", alignItems:"center",justifyContent:"center"}}>
+      Welcome to React E-commerce App!!
+    </div>
+  );
+}
+
+export default App;
